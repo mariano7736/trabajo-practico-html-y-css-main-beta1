a210608c224e9093d8b4fbc28ee2a0d35f6ed6e2
ramirorinaldi <r.rinaldi@guideline-contractors.com>
2023-10-26 01:49:29 +0200

    ajustes

--- trabajo-practico-html-y-css-main/script/app2.js
+++ trabajo-practico-html-y-css-main/script/app2.js
@@ -1,50 +1,44 @@
-window.addEventListener('load', ()=> {
-    let lon
-    let lat
+window.addEventListener('load', () => {
+  let lon
+  let lat
 
-    let temperaturaValor = document.getElementById('temperatura-valor')  
-    let temperaturaDescripcion = document.getElementById('temperatura-descripcion')  
-    
-    let ubicacion = document.getElementById('ubicacion')  
-   
+  let temperaturaValor = document.getElementById('temperatura-valor')
+  let temperaturaDescripcion = document.getElementById('temperatura-descripcion')
 
-    let vientoVelocidad = document.getElementById('viento-velocidad') 
+  let ubicacion = document.getElementById('ubicacion')
 
 
-    if(navigator.geolocation){
-       navigator.geolocation.getCurrentPosition( posicion => {
-           lon = posicion.coords.longitude
-           lat = posicion.coords.latitude
-           
+  let vientoVelocidad = document.getElementById('viento-velocidad')
 
-           //ubicación por ciudad
-           const url = `https://api.openweathermap.org/data/2.5/weather?q=Buenos Aires&lang=es&units=metric&appid=47d75c96e41fc2eed5aeb1d315c351ae`
 
-           
+  if (navigator.geolocation) {
 
-           fetch(url)
-            .then( response => { return response.json()})
-            .then( data => {
-               
-                
-                let temp = Math.round(data.main.temp)
-              
-                temperaturaValor.textContent = `${temp} ° C`
 
-                
-                let desc = data.weather[0].description
-                temperaturaDescripcion.textContent = desc.toUpperCase()
-                ubicacion.textContent = data.name
-                
-                vientoVelocidad.textContent = `${data.wind.speed} m/s`
-                
+    //ubicación por ciudad
+    const url = `https://api.openweathermap.org/data/2.5/weather?q=Buenos%20Aires&lang=es&units=metric&appid=47d75c96e41fc2eed5aeb1d315c351ae`
 
-                
-            })
-            .catch( error => {
-                console.log(error)
-            })
-       })
-          
-    }
+
+    fetch(url)
+      .then(response => {
+        return response.json()
+      })
+      .then(data => {
+        let temp = Math.round(data.main.temp)
+
+        temperaturaValor.textContent = `${temp} ° C`
+
+
+        let desc = data.weather[0].description
+        temperaturaDescripcion.textContent = desc.toUpperCase()
+        ubicacion.textContent = data.name
+
+        vientoVelocidad.textContent = `${data.wind.speed} m/s`
+
+
+      })
+      .catch(error => {
+        console.log(error)
+      })
+
+  }
 })
